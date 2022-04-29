7908e6dfd3eb84e9d9bc56948dbf82767384cba9
Oliver-makes-code <kookooloopdaking@gmail.com>
2022-04-29 09:29:50 -0500

    Set up webview for client

---
+++ run.sh
@@ -0,0 +1,1 @@
+deno run --allow-all --unstable src/client/mod.ts
---
+++ src/client/mod.ts
@@ -0,0 +1,13 @@
+import { Webview } from "https://deno.land/x/webview@0.7.0-pre.1/mod.ts";
+
+const html = `
+  <html>
+  <body>
+    <h1>Hello from deno v${Deno.version.deno}</h1>
+  </body>
+  </html>
+`;
+
+const webview = new Webview();
+webview.navigate(`data:text/html,${encodeURIComponent(html)}`);
+webview.run();
