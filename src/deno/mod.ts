442ce4c9975633edb361e3d3feda5e9175c68885
Oliver-makes-code <kookooloopdaking@gmail.com>
2022-05-02 10:44:41 -0500

    Actually there's no need for xon

--- src/deno/mod.ts
+++ src/deno/mod.ts
@@ -1,8 +1,5 @@
 import { Webview } from "https://deno.land/x/webview@0.7.0-pre.1/mod.ts";
-import { XON } from "./xon/index.ts";
-
-const html = XON.parse(await Deno.readTextFile("./src/deno/index.xns"));
 const webview = new Webview();
-webview.navigate(`data:text/html,${encodeURIComponent(html)}`);
+webview.navigate(`data:text/html,${encodeURIComponent("")}`);
 webview.title = "Mineosaur Client"
 webview.run();
--- src/deno/xon/index.ts
+++
@@ -1,3 +0,0 @@
-import XonParser from "./XonParser.ts";
-export const XON = new XonParser();
-export type { XMLDoc, XMLNode } from "./types.ts";
